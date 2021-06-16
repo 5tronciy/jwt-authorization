6d5c3b1851dfa039c3c6bdb033e5b6944d58b88a
5tronciy <62604867+5tronciy@users.noreply.github.com>
2021-06-16 23:20:57 +0300

    finish registration

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ const start = async () => {
     await mongoose.connect(process.env.DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true,
     });
     app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
   } catch (e) {
